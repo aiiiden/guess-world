8c54e48be662aa8dcb34770d25a4c3af9af5b56e
aiiiden <hjjam100@gmail.com>
2023-12-15 20:18:53 +0900

    Init Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Guess World",
+  description: "Guess & Reveal",
+  robots: "noindex, nofollow",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
